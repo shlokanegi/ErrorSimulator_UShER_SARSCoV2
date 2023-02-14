c422b542cd53cb8534f30ca3baae81c74898df97
Shloka Negi <66521525+shlokanegi@users.noreply.github.com>
2023-02-14 14:43:28 -0800

    Update errorSimulator.v3.py

--- errorSimulator.v3.py
+++ errorSimulator.v3.py
@@ -69,34 +69,33 @@ def dfs_traversal_and_error_addition(current_node, leaf_ids, sample_leaf_nodes, 
 
     ## Incorporating random errors into the sampled leaf nodes
     if current_node.id in sample_leaf_nodes:
-        
         num_errors = sample_leaf_nodes[current_node.id]
         if(num_errors != 0):
-        print(f"No. of mutations for nodeID {current_node.id} is {len(current_node.mutations)}.\nNo. of errors to be incorporated: {num_errors}")
-        current_node_mutations = current_node.mutations
+            # print(f"No. of mutations for nodeID {current_node.id} is {len(current_node.mutations)}.\nNo. of errors to be incorporated: {num_errors}")
+            current_node_mutations = current_node.mutations
 
-        error_transition_matrix = np.zeros((4, 4))
-        for i in range(num_errors): # Randomly incorporate `num_errors` errors in current_node
-            while True:
-                error_site_idx = np.random.randint(len(reference_genome))   # choose error site `error_site_idx`
-                if current_node_DNA_sequence[error_site_idx] == reference_genome[error_site_idx]:   # means this site is non-mutated
-                    current_ref_base = current_node_DNA_sequence[error_site_idx]
-                    ## Choosing current_alt_base based on transition probability matrix
-                    transition_counts = transition_matrix[base_to_idx_mapping[current_ref_base]]
-                    transition_counts = transition_counts / transition_counts.sum()
-                    current_alt_base = np.random.choice(['A', 'T', 'C', 'G'], p=transition_counts)   # we have sampled the `current_alt_base` using the transition probability matrix
-                    error_transition_matrix[base_to_idx_mapping[current_ref_base]][base_to_idx_mapping[current_alt_base]] += 1
-                    current_node_mutations.append(''.join(["NC_045512v2:", current_ref_base, str(error_site_idx), current_alt_base]))
-                    # current_node_mutations.append(''.join([current_ref_base, str(error_site_idx), current_alt_base]))
-                    break
+            error_transition_matrix = np.zeros((4, 4))
+            for i in range(num_errors): # Randomly incorporate `num_errors` errors in current_node
+                while True:
+                    error_site_idx = np.random.randint(len(reference_genome))   # choose error site `error_site_idx`
+                    if current_node_DNA_sequence[error_site_idx] == reference_genome[error_site_idx]:   # means this site is non-mutated
+                        current_ref_base = current_node_DNA_sequence[error_site_idx]
+                        ## Choosing current_alt_base based on transition probability matrix
+                        transition_counts = transition_matrix[base_to_idx_mapping[current_ref_base]]
+                        transition_counts = transition_counts / transition_counts.sum()
+                        current_alt_base = np.random.choice(['A', 'T', 'C', 'G'], p=transition_counts)   # we have sampled the `current_alt_base` using the transition probability matrix
+                        error_transition_matrix[base_to_idx_mapping[current_ref_base]][base_to_idx_mapping[current_alt_base]] += 1
+                        current_node_mutations.append(''.join(["NC_045512v2:", current_ref_base, str(error_site_idx), current_alt_base]))
+                        # current_node_mutations.append(''.join([current_ref_base, str(error_site_idx), current_alt_base]))
+                        break
 
-        print(f"Errors added: {current_node_mutations[-num_errors:]}")
-        # print("Non-updated mutation list: ", current_node.mutations)
-        current_node.update_mutations(current_node_mutations)
-        # print("Updated mutation list: ", current_node.mutations)
-        transition_matrix = transition_matrix + error_transition_matrix
-        print(f"No. of mutations after error addition: {len(current_node.mutations)}")
-        print()
+            # print(f"Errors added: {current_node_mutations[-num_errors:]}")
+            # print("Non-updated mutation list: ", current_node.mutations)
+            current_node.update_mutations(current_node_mutations)
+            # print("Updated mutation list: ", current_node.mutations)
+            transition_matrix = transition_matrix + error_transition_matrix
+            # print(f"No. of mutations after error addition: {len(current_node.mutations)}")
+            print()
         
    
   ## If current_node is not a leaf node
@@ -127,7 +126,9 @@ def reversion_addition(leaf_node_list):
         mutations_root_to_leaf = list(mat.get_haplotype(leaf_node.id))
         num_reversions_current_leaf = binom.rvs(n=len(mutations_root_to_leaf), p=reversion_error_rate)  # check this, should it be (reversion_error_rate / 100) ?
         reversion_indices_list = list(np.random.randint(len(mutations_root_to_leaf), size=num_reversions_current_leaf))
+        print(f"No. of reversions to be added: {len(reversion_indices_list)}")
         current_leaf_mutations = leaf_node.mutations
+        print(f"No. of mutations of leaf before adding reversions: {len(current_leaf_mutations)}")
         for idx in reversion_indices_list:
             mutation = mutations_root_to_leaf[idx]
             current_ref_base = mutation[0]
@@ -135,6 +136,7 @@ def reversion_addition(leaf_node_list):
             current_alt_base = mutation[-1]
             current_leaf_mutations.append(''.join(["NC_045512v2:", current_alt_base, str(error_site_idx), current_ref_base]))
         
+        print(f"No. of mutations of leaf after adding reversions: {len(current_leaf_mutations)}")
         leaf_node.update_mutations(current_leaf_mutations)
         return
 
@@ -150,7 +152,7 @@ def main():
     expected_error_count = (error_rate * tree_mutation_count)/100
     error_count = poisson.rvs(expected_error_count)
 
-    print(f"Total number of errors being incorporated: {error_count}\n")
+    # print(f"Total number of errors being incorporated: {error_count}\n")
     leaf_node_list = mat.get_leaves()
 
     reversion_addition(leaf_node_list)
@@ -171,7 +173,7 @@ def main():
     transition_matrix = np.ones((4, 4))
     dfs_traversal_and_error_addition(mat.root, leaf_ids, sample_leaf_nodes, sequence, transition_matrix, base_to_idx_mapping, reference_genome)  
 
-    mat.write_vcf(vcf_file = "subtree_errors.vcf") #Write into VCF - with original mutations + errors
+    mat.write_vcf(vcf_file = "subtree_errors_reversions.vcf") #Write into VCF - with original mutations + errors + reversions
     return 
 
 
@@ -187,9 +189,7 @@ parser.add_argument('-rev', '--reversion', type=float, metavar='', nargs='?', co
 parser.add_argument('-ad', '--amplicon_drop', type=float, metavar='', nargs='?', const=0, default=0, help='Amplicon dropout rate, DEFAULT: 0')
 args = parser.parse_args()
 
-# mat = bte.MATree(pb_file = '/home/shloka/data/phastSim_output_scaled/sars-cov-2_simulation_output.mat.pb') # with phastSim scale parameter = 0.1
 mat = bte.MATree(args.tree)
-# error_rate = 0.1 # in percentage (will be taken as input)
 error_rate = args.random
 reversion_error_rate = args.reversion
 amplicon_dropout_rate = args.amplicon_drop
